078ca4c3900d5818b0e767618667921e81a15a79
Klayton Dias <arqkdias@gmail.com>
2023-06-23 09:34:47 -0400

    -

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){  //condição simples
+    console.log(`Você ultrapassou a velocidade perimitida: Multado`)
+}
+console.log(`dige sempre usando cinto`)
