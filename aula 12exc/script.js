9e9320a48dd2a72b33da3b428badbcd01cc141da
Klayton Dias <arqkdias@gmail.com>
2023-06-26 10:44:15 -0400

    -

--- aula 12exc/script.js
+++ aula 12exc/script.js
@@ -1,19 +1,31 @@
 
 var titulo = document.getElementById('titulo_pagina')
 var primiera_linha = document.getElementById('linha1')
+var imagem = document.getElementById('imagem')
+var body = document.body;
 var agora = new Date()
 var hora = agora.getHours()
-primiera_linha.innerHTML = `Agora são exatamente ${hora} horas`
+primiera_linha.innerHTML = `<strong>Agora são ${hora} horas</strong>`
 
 if (hora < 12 && hora > 5 ) {
     titulo.innerHTML  = 'Bom dia!!'
+    imagem.src = "manha.jpg";
+    body.style.backgroundColor = "#d8d1c5"
+
 } else if (hora < 18 && hora > 12) {
     titulo.innerHTML = 'Boa Tarde!!'
+    imagem.src = "tarde.jpg";
+    body.style.backgroundColor = "#42755d"
+
 } else if (hora < 23 && hora > 18) {
+    titulo.innerHTML = 'Boa Noite!!'
+    imagem.src = "noite.jpg";
+    body.style.backgroundColor = "#191970"
 
 } else {
     titulo.innerHTML = ('Eita ja esta de madrugada')
+    imagem.src = "madrugada.jpg";
+    body.style.backgroundColor = "#2c6c8b"
 }
 
 
- 
